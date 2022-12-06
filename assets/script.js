5350d279c26c0ca8bff6f3ff585a02b00a652e67
Samantha Leyva <samanthaleyva@Samanthas-MacBook-Air-2.local>
2022-12-05 20:59:45 -0600

    solved local storage issue, still working on how to check if a city will work, styling almost done, need to work on wireframe

--- assets/script.js
+++ assets/script.js
@@ -8,24 +8,25 @@ let cityDiv = document.querySelector("#city-weather");
 let forecastDiv = document.querySelector("#forecast");
 let historyDiv = document.querySelector("#past-searches");
 
-let pastSearches = [];
+let pastSearches = JSON.parse(localStorage.getItem("cities")) || [];
 
 // functions
+
 function init() {
-  console.log("pastSearches at init", pastSearches);
   displayHistory();
 }
 
 function displayHistory() {
   // show past searches
-  let history = JSON.parse(localStorage.getItem("cities")) || [];
-  console.log("history in display history", history);
-  if (history.length > 0) {
-    history.forEach((search) => {
-      let historyBtn = document.createElement("button");
-      historyBtn.innerHTML = search;
-      historyBtn.classList.add("historyBtn");
-      btnsDiv.append(historyBtn);
+  historyDiv.innerHTML = "";
+  if (pastSearches.length > 0) {
+    pastSearches.forEach((search) => {
+      if (search != "") {
+        let historyBtn = document.createElement("button");
+        historyBtn.innerHTML = search;
+        historyBtn.classList.add("historyBtn");
+        historyDiv.append(historyBtn);
+      }
     });
   } else {
     return;
@@ -37,11 +38,8 @@ function getWeather(city) {
   cityDiv.innerHTML = "";
   forecastDiv.innerHTML = "";
 
-  // add function that checks if the city exists
+  handlePastSearches(city);
 
-  pastSearches.push(city);
-  console.log("pastSearches at getWeather", pastSearches);
-  localStorage.setItem("cities", JSON.stringify(pastSearches));
   let forecastURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${APIKey}&units=metric`;
   fetch(forecastURL)
     .then(function (response) {
@@ -51,37 +49,59 @@ function getWeather(city) {
       console.log("data", data);
       // Current City Weather
       let iconURL = `https://api.openweathermap.org/img/w/${data.list[0].weather[0].icon}`;
-      cityDiv.innerHTML += `<h1>${data.city.name}</h1> <img src="${iconURL}" alt="">`;
-      cityDiv.innerHTML += data.list[0].dt_txt.split(" ")[0];
-      cityDiv.innerHTML += `Temperature: ${data.list[0].main.temp}`;
-      cityDiv.innerHTML += `Wind: ${data.list[0].wind.speed}MPH`;
-      cityDiv.innerHTML += `Humidity: ${data.list[0].main.humidity}%`;
+      let cityBox = document.createElement("div");
+      cityBox.classList.add("weatherBox");
+      cityBox.innerHTML += `<h2 class="cityName">${data.city.name}</h2> <img class="icon" src="${iconURL}" alt="">`;
+      cityBox.innerHTML += `<h3 class="weatherDate">${
+        data.list[0].dt_txt.split(" ")[0]
+      }</h3>`;
+      cityBox.innerHTML += `<h3>Temperature: ${data.list[0].main.temp}</h3>`;
+      cityBox.innerHTML += `<h3>Wind: ${data.list[0].wind.speed}MPH</h3>`;
+      cityBox.innerHTML += `<h3>Humidity: ${data.list[0].main.humidity}%</h3>`;
+      cityDiv.appendChild(cityBox);
       // 5-Day Forecast
       data.list.forEach((timeSec, index) => {
         let time = data.list[index].dt_txt.split(" ")[1];
         let date = data.list[index].dt_txt.split(" ")[0];
         if (time === "00:00:00") {
           let iconURL = `https://api.openweathermap.org/img/w/${data.list[index].weather[0].icon}`;
-          forecastDiv.innerHTML += `<img src="${iconURL}" alt="">`;
-          forecastDiv.innerHTML += `<h2>${date}</h2>`;
-          forecastDiv.innerHTML += `<h2>Temperature: ${data.list[index].main.temp}</h2>`;
-          forecastDiv.innerHTML += `<h2>Wind: ${data.list[0].wind.speed} MPH</h2>`;
-          forecastDiv.innerHTML += `<h2>Humidity: ${data.list[0].main.humidity} %</h2>`;
+          let forecastBox = document.createElement("div");
+          forecastBox.classList.add("weatherBox");
+          forecastBox.innerHTML += `<h3 class="forecastDate">${date}</h3> <img class="forecastIcon" src="${iconURL}" alt="">`;
+          forecastBox.innerHTML += `<h3 class="forecastInfo">Temperature: ${data.list[index].main.temp}</h3>`;
+          forecastBox.innerHTML += `<h3 class="forecastInfo">Wind: ${data.list[0].wind.speed} MPH</h3>`;
+          forecastBox.innerHTML += `<h3 class="forecastInfo">Humidity: ${data.list[0].main.humidity} %</h3>`;
+          forecastDiv.appendChild(forecastBox);
         }
       });
     });
 }
 
+function handlePastSearches(city) {
+  // how do I check that there's actually a city that you can search for?
+  // if the city is already in the array, don't want it again
+  if (pastSearches.includes(city) === false) {
+    pastSearches.push(city);
+    console.log("pastSearches in handlePastSearches - false", pastSearches);
+    localStorage.setItem("cities", JSON.stringify(pastSearches));
+  }
+}
+
 // event listener
 init();
 btnsDiv.addEventListener("click", function (e) {
+  e.preventDefault();
   if (e.target.classList.contains("historyBtn")) {
     let cityChoice = e.target.innerHTML;
-    console.log("hopefully prev buton", cityChoice);
     getWeather(cityChoice);
-  } else {
+    displayHistory();
+  } else if (e.target.classList.contains("searchBtn")) {
     let cityChoice = cityName.value;
-    console.log("hopefully user input", cityChoice);
-    getWeather(cityChoice);
+    if (cityChoice != "") {
+      getWeather(cityChoice);
+      displayHistory();
+    }
+  } else {
+    return;
   }
 });
